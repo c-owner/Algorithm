948aab007b7038759dc9699032e7d469f7783a50
Eight-Corner <corner3499@gmail.com>
2020-11-14 17:16:25 +0900

    시간에 3이 들어갈 때마다 60초 더하기

--- Algorithm/src/Lv1/ex6.java
+++ Algorithm/src/Lv1/ex6.java
@@ -2,9 +2,32 @@ package Lv1;
 
 public class ex6 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static int time3 = 0; // 3이 나오는 시간을 카운트
 
+	static void digitalMethod() {
+		for (int i = 0; i < 24; i ++ ) {
+			if(i == 3 || i == 13 || i == 23 ) {
+				time3+=60; // 60분으로
+				continue;
+			} else for(int m = 0; m < 6; m++) {
+				if(m == 3 ) {
+					time3+= 10;
+					continue;
+				} else {
+					time3++;
+				}
+			}
+		}
+		System.out.println("3이 있는 총 60초의 시간은");
+		System.out.println(time3*60+"초 입니다.");
+	}
+	
+	public static void main(String[] args) {
+
+		System.out.println();
+		digitalMethod();
+		
+		
 	}
 
 }
