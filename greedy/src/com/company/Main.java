365f3bb8b8de67bc453dbd3ee8654fb9a5c45f56
Eight-Corner <corner3499@gmail.com>
2021-02-18 00:03:27 +0900

    hello

---
+++ greedy/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("my world");
+        System.out.println("intelliJ iDEA");
+    }
+}
