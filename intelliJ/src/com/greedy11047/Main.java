26a42bd5d7979c07c44d759da692dff8dac0535f
Eight-Corner <corner3499@gmail.com>
2021-02-21 00:08:17 +0900

    baekjoon-11047

--- intelliJ/src/com/greedy11047/Main.java
+++ intelliJ/src/com/greedy11047/Main.java
@@ -5,38 +5,37 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("> ");
+        System.out.print("동전의 종류 개수 입력 > ");
         int n = sc.nextInt();
+
         int[] nArr = {1,5,10,50,100,500,1000,5000,10000,50000};
+
         int cnt = 0; // 최소 동전개수
         System.out.print("> ");
         int k = sc.nextInt(); // 가치의 합
+        int i = 10;
 
-        if( n < 1 && n > 10 ) {
-            System.exit(0);
-        }
+            for(int j = 0; j < n; j++  ) {
+                System.out.println(nArr[j]);
+            }
+        if( n >= 1 && n <= 10 ) {
 
-            while(k > 0 ) {
-                int i = 10;
-                if(k % nArr[i-1] == 0 ){
-                   k -= nArr[i-1];
+            while (k > 0) {
+                if ( k >= nArr[n-1]) {
+                    k -= nArr[n - 1];
                     cnt++;
-                } else {
-                    i--;
                 }
-                if( k == 0 ) {
+               if ( k < nArr[n - 1]) {
+                   n--;
+               }
+
+                if (k == 0) {
                     break;
                 }
+
             }
-
+        }
         System.out.println(cnt);
-
-
-
-
-
-
-
 
     }
 }
