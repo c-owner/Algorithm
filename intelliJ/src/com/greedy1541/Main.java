c7858a87bcabcbb88a41f919477634e4cb0823c0
Eight-Corner <corner3499@gmail.com>
2021-03-21 21:54:20 +0900

    📝1541-잃어버린 괄호

--- intelliJ/src/com/greedy1541/Main.java
+++ intelliJ/src/com/greedy1541/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     static int min = 0;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
@@ -11,22 +12,18 @@ public class Main {
         String[] str = input.split("-");
         min += sum(str[0].split("[+]"));
 
-        for (int i = 1; i < str.length; i++ ) {
-         min -= sum(str[i].split("[+]"));
-            System.out.println("min = "+min);
+        for (int i = 1; i < str.length; i++) {
+            min -= sum(str[i].split("[+]"));
         }
-        System.out.println(min);
 
 
     }
 
     static int sum(String[] input) {
         int result = 0;
-        for (String num : input ) {
+        for (String num : input) {
             result += Integer.parseInt(num);
-            System.out.println("num = " + num);
-            }
-        System.out.println("result = " + result);
+        }
         return result;
     }
 
